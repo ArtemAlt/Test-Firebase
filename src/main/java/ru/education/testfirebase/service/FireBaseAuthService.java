62e0ecdab5903781ce2613211c28365310f1a4bc
ArtemAlt <artem.altunin.vit@gmail.com>
2021-05-17 20:40:44 +0300

    Hello

--- src/main/java/ru/education/testfirebase/service/FireBaseAuthService.java
+++ src/main/java/ru/education/testfirebase/service/FireBaseAuthService.java
@@ -1,16 +1,12 @@
 package ru.education.testfirebase.service;
 
 import com.google.api.client.json.webtoken.JsonWebSignature;
-import com.google.api.core.ApiFuture;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.google.firebase.auth.UserRecord;
 import com.google.firebase.auth.internal.FirebaseCustomAuthToken;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.remoteconfig.User;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +16,8 @@ import java.util.concurrent.ExecutionException;
 @RequiredArgsConstructor
 public class FireBaseAuthService {
 
-    private final FirebaseAuth auth = FirebaseAuth.getInstance();
-    private final FirebaseMessaging messaging = FirebaseMessaging.getInstance();
-
-    public String createNewAuthUser(String number,String email) throws FirebaseAuthException, FirebaseMessagingException {
+    public String createNewAuthUser(String number,String email) throws FirebaseAuthException {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
         UserRecord.CreateRequest request = new UserRecord.CreateRequest()
                 .setEmail(email)
                 .setEmailVerified(false)
@@ -37,6 +31,7 @@ public class FireBaseAuthService {
         return "Successfully created new user: " + userRecord.getUid();
     }
     public boolean verificationToken(String token){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
         try {
             return auth.verifyIdToken(token) != null;
         } catch (FirebaseAuthException e) {
@@ -44,13 +39,15 @@ public class FireBaseAuthService {
         }return false;
     }
 
-    public String checkToken (String token) throws FirebaseAuthException, ExecutionException, InterruptedException {
+    public String checkToken (String token) throws FirebaseAuthException{
+        FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseToken response=  auth.verifyIdToken(token);
        return response.getUid();
 
     }
 
     public String createToken(String uid) throws FirebaseAuthException {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
         UserRecord record=  auth.getUser(uid);
         System.out.println(record.toString());
         record.getPhoneNumber();
