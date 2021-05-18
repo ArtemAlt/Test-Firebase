603d8c2fdd7a73da47a4ccfb9dea82d00725b61b
ArtemAlt <artem.altunin.vit@gmail.com>
2021-05-18 12:03:32 +0300

    fix

--- src/main/java/ru/education/testfirebase/FireBaseInitializer.java
+++ src/main/java/ru/education/testfirebase/FireBaseInitializer.java
@@ -3,17 +3,19 @@ package ru.education.testfirebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-@Service
+@Configuration
 public class FireBaseInitializer {
 
-    @PostConstruct
-    private void initFB() throws IOException {
+    @Bean
+    void initFB() throws IOException {
         try (FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-account.json")) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
